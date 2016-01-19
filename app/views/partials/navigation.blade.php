49b3b24b96a44d5b126d631a174daa5ee7d7c6aa
Sakib Shaikh <sshaikh210@hotmail.com>
2016-01-19 14:43:33 -0600

    Deleted extra CSS files, and updated Navigation bar file

--- app/views/partials/navigation.blade.php
+++ app/views/partials/navigation.blade.php
@@ -1,31 +1,3 @@
-<html>
-<head>
-    <title>Nav Bar</title>
-        <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Custom CSS -->
-    <link href="/css/clean-blog.min.css" rel="stylesheet">
-
-    <!-- Bootstrap Core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Fonts -->
-    <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="/css/animate.css" rel="stylesheet" />
-
-    <!-- Custom Fonts -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <!-- Squad theme CSS -->
-    <link href="/css/style.css" rel="stylesheet">
-    <link href="/color/default.css" rel="stylesheet">
-</head>
-<body>       
-
     <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header page-scroll">
@@ -61,14 +33,3 @@
         </div>
         <!-- /.container -->
     </nav>
-
-        <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/jquery.easing.min.js"></script>    
-    <script src="/js/jquery.scrollTo.js"></script>
-    <script src="/js/wow.min.js"></script>
-    <!-- Custom Theme JavaScript -->
-    <script src="/js/custom.js"></script>
-    @yield('bottom-script')
-    </body>
-</html> 
