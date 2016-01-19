7d28b7527dbfe10e632a5f1c9a556fadda84554a
Roger Chin <roger.sc.chin@gmail.com>
2016-01-19 13:24:26 -0600

    Basic Setup for Confide and Entrust. Confide will create the migration for a users table. Entrust will create a table for Roles and Permission. Entrust will also create the models for Role and Permission. User class is modified to implement the Confide and Entrust interface.

--- app/models/User.php
+++ app/models/User.php
@@ -4,10 +4,17 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Zizaco\Confide\ConfideUser;
+use Zizaco\Confide\ConfideUserInterface;
+use Bbatsche\Entrust\Contracts\EntrustUserInterface;
+use Bbatsche\Entrust\Traits\EntrustUserTrait;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+class User extends Eloquent implements ConfideUserInterface, EntrustUserInterface {
+
+	// use UserTrait, RemindableTrait;
+	use ConfideUser;
+	use EntrustUserTrait;
 
 	/**
 	 * The database table used by the model.
@@ -24,3 +31,6 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	protected $hidden = array('password', 'remember_token');
 
 }
+
+
+
