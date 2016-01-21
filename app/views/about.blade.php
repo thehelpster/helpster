cf8e2ec1fd0d9bf53b99b1d68a239bedeb1373a0
Sakib Shaikh <sshaikh210@hotmail.com>
2016-01-21 15:08:55 -0600

    About blade file

---
+++ app/views/about.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+                <div class="col-md-4">
+                  <div class="service-features">
+                  <div class="img-container">
+                    <img src="images/paperplane.png" alt="Service Features">
+                  </div>
+                    <h3>Volunteers</h3>
+                    <p>If you are looking to make an impact in the world starting with your own community, Helpster is where to start. We will show you where your city can use your help, all you have to do is show up</p>
+                  </div>
+                </div><!-- END ONE THIRD COLUMN -->
+
+                <div class="col-md-4">
+                  <div class="service-features service-features2">
+                  <div class="img-container">
+                    <img src="images/lab.png" alt="Service Features">
+                  </div>
+
+                    <h3>Organizations</h3>
+                    <p>If you are an established non-profit organization and have work that needs to be done but can’t find the help, we are here for you. Our database of volunteers will expand the globe and you will be able to identify your volunteers by your needs.</p>
+                  </div>
+
+                </div><!-- END ONE THIRD COLUMNS -->
+
+                <div class="col-md-4">
+                  <div class="service-features">
+                  <div class="img-container">
+                    <img src="images/like.png" alt="Service Features">
+                  </div>
+                    <h3>Events</h3>
+                    <p>Our events are the grandest display of empathy on Earth. If you need help, we will host an event for you with people who can help you with whatever you need.</p>
+                  </div>
+
+                </div><!-- END ONE THIRD COLUMNS -->
+
+
+
+
+            </div>
+@stop
