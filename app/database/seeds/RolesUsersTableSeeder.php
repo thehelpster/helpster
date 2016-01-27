4ba0e51e9f76b89734f423b23a114cced802d686
Roger Chin <roger.sc.chin@gmail.com>
2016-01-27 10:23:34 -0600

    Modified seeders to have false info for admin and volunteer to test out more functions

--- app/database/seeds/RolesUsersTableSeeder.php
+++ app/database/seeds/RolesUsersTableSeeder.php
@@ -3,9 +3,33 @@
 class RolesUsersTableSeeder extends Seeder {
 	public function run()
 	{
-		$rolesuser = new RolesUser;
-		$rolesuser->user_id = '1';
-		$rolesuser->role_id = '1';
-		
+		$roleuser = new RolesUser;
+		$roleuser->user_id = '1';
+		$roleuser->role_id = '1';
+		$roleuser->save();
+
+		//assigning Jerald the role of 1 which is 'owner'
+		$jerald = new RolesUser;
+		$jerald->user_id = '2'; 
+		$jerald->role_id = '1';
+		$jerald->save();
+
+		//assigning Sakib the role of 1 which is 'owner'
+		$sakib = new RolesUser;
+		$sakib->user_id = '3';
+		$sakib->role_id = '1';
+		$sakib->save();
+
+		//assigning $testAdmin the role of 2 which is 'admin'
+		$testAdmin = new RolesUser;
+		$testAdmin->user_id = '4';
+		$testAdmin->role_id = '2';
+		$testAdmin->save();
+
+		//assigning $testVolunteer the role of 3 which is 'volunteer'
+		$testVolunteer = new RolesUser;
+		$testVolunteer->user_id = '5';
+		$testVolunteer->role_id = '3';
+		$testVolunteer->save();
 	}
 }
