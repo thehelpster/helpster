13a571b3ad9c3c3aefd2dd051102577bf3592722
Sakib Shaikh <sshaikh210@hotmail.com>
2016-01-28 11:07:57 -0600

    ability to view users profiles from events list has been added

--- app/views/events/users.blade.php
+++ app/views/events/users.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-	<div class="container">
-		<table class="table table-bordered table-striped">
+	<div class="container table-responsive">
+		<table class="table">
 			<tr>
 				<th class="col-md-1">Image</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-				<th class="col-md-1">E-Mail</th>
+				<th class="col-md-4">Name</th>
+				<th class="col-md-4">E-Mail</th>
 			</tr>
 			@foreach ($rsvps->rsvps as $rsvp)
 			
 				<tr class='table table-hover table-bordered body'>
 					<td><img src="{{{$rsvp->image}}}"  class="img-responsive"></td>
-					<td>{{{$rsvp->first_name}}}</td>
-					<td>{{{$rsvp->last_name}}}</td>  
+					<td><a href="{{{ action('VolunteerController@viewUser', $rsvp->id) }}}">{{{$rsvp->first_name}}} {{{$rsvp->last_name}}}</a></td>  
 					<td>{{{$rsvp->email}}}</td>
 				</tr>
 			@endforeach
